aa61899b997419ba9dc78e2bf28d13bc583c06c7
Mike Dickson <mike.dickson@readinghorizons.com>
2019-10-11 16:40:24 -0600

    Initial Stuff for realz this time

---
+++ scripts/game.js
@@ -0,0 +1,20 @@
+window.onload();
+
+var deck;
+
+function BuildDeck() {
+     deck = [ "AceSpades", "2Spades", "3Spades", "4Spades", "5Spades", "6Spades", "7Spades", "8Spades", "9Spades", "10Spades", "JackSpades", "QueenSpades", "KingSpades",
+            "AceClubs", "2Clubs", "3Clubs", "4Clubs", "5Clubs", "6Clubs", "7Clubs", "8Clubs", "9Clubs", "10Clubs", "JackClubs", "QueenClubs", "KingClubs",
+            "AceHearts", "2Hearts", "3Hearts", "4Hearts", "5Hearts", "6Hearts", "7Hearts", "8Hearts", "9Hearts", "10Hearts", "JackHearts", "QueenHearts", "KingHearts",
+            "AceDiamonds", "2Diamonds", "3Diamonds", "4Diamonds", "5Diamonds", "6Diamonds", "7Diamonds", "8Diamonds", "9Diamonds", "10Diamonds", "JackDiamonds", "QueenDiamonds", "KingDiamonds"];
+}
+
+function ShuffleDeck() {
+       
+    deck.sort(function() {
+      return .5 - Math.random();
+    });
+    
+    //console.log(deck);
+    
+}
