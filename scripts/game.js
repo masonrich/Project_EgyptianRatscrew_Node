e1dd71d532ae17cdb2b2b5b15ffe7291d756658e
mike <mike.dickson@readinghorizons.com>
2019-10-14 21:25:18 -0600

    Added logic to start game and play cards

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,8 @@
-window.onload();
+//window.onload();
 
-var deck;
+var deck, player1Deck, player2Deck, pile, playerTurn;
+
+//0 is player1 and 1 is player 2. We toggle 0 and 1 to see who's turn it is.
 
 function BuildDeck() {
      deck = [ "AceSpades", "2Spades", "3Spades", "4Spades", "5Spades", "6Spades", "7Spades", "8Spades", "9Spades", "10Spades", "JackSpades", "QueenSpades", "KingSpades",
@@ -16,5 +18,60 @@ function ShuffleDeck() {
     });
     
     //console.log(deck);
-    
+}
+
+
+function DealDeck() {
+	player1Deck = new Array();
+	player2Deck = new Array();
+	
+	for (let i = 0; i < 26; i++) {
+		player1Deck.push(deck[deck.length - 1]);
+		deck.pop();
+	}
+	
+	for (let i = 0; i < 26; i++) {
+	player2Deck.push(deck[deck.length - 1]);
+	deck.pop();
+	}
+	
+	console.log(player1Deck);
+	console.log(player2Deck);
+}
+
+function StartGame() {
+	BuildDeck();
+	ShuffleDeck();
+	DealDeck();
+	
+	pile = new Array();
+	playerTurn = 0;
+}
+
+function PlayCard() {
+	if (playerTurn === 0) {	
+	
+		if (player1Deck.length === 0) {
+			//game over
+			return;
+		}
+		
+		pile.splice(0, 0, player1Deck[player1Deck.length - 1]);
+		player1Deck.pop();
+		playeTurn = 1;
+	} else {
+		
+		pile.splice(0, 0, player2Deck[player2Deck.length - 1]);
+		player2Deck.pop();
+		playerTurn = 0;
+		
+		if (player2Deck.length === 0) {
+			//game over
+			return;
+		}
+	}
+	
+	console.log(player1Deck);
+	console.log(player2Deck);
+	console.log(pile);
 }
