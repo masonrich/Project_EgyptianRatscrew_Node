409990f15fe5fd8498abb30c54a2fcb9e62b0048
alec combe <ebmoccela@gmail.com>
2019-10-14 21:20:07 -0600

    added whoFirst function

--- scripts/game.js
+++ scripts/game.js
@@ -2,11 +2,23 @@ window.onload();
 
 var deck;
 
+
 function BuildDeck() {
-     deck = [ "AceSpades", "2Spades", "3Spades", "4Spades", "5Spades", "6Spades", "7Spades", "8Spades", "9Spades", "10Spades", "JackSpades", "QueenSpades", "KingSpades",
+     deck = [ "AceSpades", "2Spades", "3Spades", "4Spades", "5Spades", "6Spades", "7Spades",            "8Spades", "9Spades", "10Spades", "JackSpades", "QueenSpades", "KingSpades",
             "AceClubs", "2Clubs", "3Clubs", "4Clubs", "5Clubs", "6Clubs", "7Clubs", "8Clubs", "9Clubs", "10Clubs", "JackClubs", "QueenClubs", "KingClubs",
             "AceHearts", "2Hearts", "3Hearts", "4Hearts", "5Hearts", "6Hearts", "7Hearts", "8Hearts", "9Hearts", "10Hearts", "JackHearts", "QueenHearts", "KingHearts",
             "AceDiamonds", "2Diamonds", "3Diamonds", "4Diamonds", "5Diamonds", "6Diamonds", "7Diamonds", "8Diamonds", "9Diamonds", "10Diamonds", "JackDiamonds", "QueenDiamonds", "KingDiamonds"];
+}
+
+//find first player at game start
+function whoFirst(){
+    var random = Math.random();
+    if(random < 0.5){
+        playerTurn = 0;
+    }
+    else{
+        playerTurn = 1;
+    }
 }
 
 function ShuffleDeck() {
