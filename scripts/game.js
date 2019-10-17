560529d1416c1a91ddd8fc10f96446c0b8971e41
mike <mike.dickson@readinghorizons.com>
2019-10-16 21:18:04 -0600

    Added some functionality for slaps

--- scripts/game.js
+++ scripts/game.js
@@ -16,7 +16,7 @@ playerTurn: counter for player turn
 
 
 function BuildDeck() {
-     deck = [ "AceSpades", "2Spades", "3Spades", "4Spades", "5Spades", "6Spades", "7Spades",            "8Spades", "9Spades", "10Spades", "JackSpades", "QueenSpades", "KingSpades",
+     deck = [ "AceSpades", "2Spades", "3Spades", "4Spades", "5Spades", "6Spades", "7Spades", "8Spades", "9Spades", "10Spades", "JackSpades", "QueenSpades", "KingSpades",
             "AceClubs", "2Clubs", "3Clubs", "4Clubs", "5Clubs", "6Clubs", "7Clubs", "8Clubs", "9Clubs", "10Clubs", "JackClubs", "QueenClubs", "KingClubs",
             "AceHearts", "2Hearts", "3Hearts", "4Hearts", "5Hearts", "6Hearts", "7Hearts", "8Hearts", "9Hearts", "10Hearts", "JackHearts", "QueenHearts", "KingHearts",
             "AceDiamonds", "2Diamonds", "3Diamonds", "4Diamonds", "5Diamonds", "6Diamonds", "7Diamonds", "8Diamonds", "9Diamonds", "10Diamonds", "JackDiamonds", "QueenDiamonds", "KingDiamonds"];
@@ -77,7 +77,8 @@ function PlayCard() {
 	if (playerTurn == 0) {	
 	
 		if (player1Deck.length == 0) {
-			//game over
+			//TODO: Accomplish the below comment
+			//game over logic
 			return;
 		}
 		
@@ -88,7 +89,8 @@ function PlayCard() {
 	} else {
 		
 		if (player2Deck.length == 0) {
-			//game over
+			//TODO: Accomplish the below comment
+			//game over logic
 			return;
 		}
 		
@@ -98,7 +100,90 @@ function PlayCard() {
 		
 	}
 	
-	console.log(player1Deck);
-	console.log(player2Deck);
-	console.log(pile);
+	IsPileSlappable();
+	
+	//console.log(player1Deck);
+	//console.log(player2Deck);
+	//console.log(pile);
 }
+
+function IsPileSlappable() {
+	if (pile.length < 2) {
+		pileCurrentlySlappable = false;
+	} else {
+		if (pile[0].substring(0, 1) == pile[1].substring(0, 1)) {
+			pileCurrentlySlappable = true;
+		} else {
+			pileCurrentlySlappable = false;
+		}
+	}
+}
+
+function slap() {
+	
+	if (pile.length == 0) {
+		return;
+	}
+		
+	if (pileCurrentlySlappable) {
+		//TODO: Accomplish the below 6 lines
+		//Need async to determine who slapped.
+		//if (player1Slapped) {
+		//	playerTurn = 0;
+		//} else {
+		//	playerTurn = 1;
+		//}
+		
+		//thie represents # of cards in pile at time of slap.
+		int length = pile.length;
+		
+		if (playerTurn == 0) {
+			for (int i = 0; i < length; i++) {
+				if (pile.length == 0) {
+					break;
+				}
+				
+				player1Deck.splice(0, 0, pile);
+				pile.pop();
+			}
+		} else {
+			for (int i = 0; i < length; i++) {
+				if (pile.length == 0) {
+					break;
+				}
+				
+				player2Deck.splice(0, 0, pile);
+				pile.pop();
+			}
+		}
+		
+	} else {
+		let whoSlapped;
+		//TODO: Accomplish the below comment
+		//Need asnyc to determine which player incorrectly slapped.
+		if (player1Slapped) {
+			whoSlapped = 0;
+		} else {
+			whoSlapped = 1;
+		}
+		
+		//current player plays 2 cards from bottom of deck to bottom of pile
+		if (whoSlapped = 0) {
+			pile.splice(pile.length - 1, 0, player1Deck[0]);
+			pile.splice(pile.length - 1, 0, player1Deck[0]);
+		} else {
+			pile.splice(pile.length - 1, 0, player2Deck[0]);
+			pile.splice(pile.length - 1, 0, player2Deck[0]);
+		}
+		
+		//check to see if this triggers end game condition
+		if (player1Deck.length == 0) {
+			//TODO: Accomplish the below comment
+			//player 2 wins and game ends
+		} else if (player2Deck.length == 0) {
+			//TODO: Accomplish the below comment
+			//player 1 wins and game ends
+		}
+	}
+}
+
